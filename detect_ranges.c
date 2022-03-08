224992e8fdf29a705e82c1cd25acf249d4bc66a3
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-08 20:52:45 +0530

    added a logic to pass the test

--- detect_ranges.c
+++ detect_ranges.c
@@ -5,6 +5,6 @@
 char* findReadingsFromRange (int *input)
 {
 	char output_string[10];
-	sprintf (output_string, "%d-%d,%d", input[0],input[1], 2)
+	sprintf (output_string, "%d-%d,%d", input[0],input[1], 2);
 	return output_string;
 }
