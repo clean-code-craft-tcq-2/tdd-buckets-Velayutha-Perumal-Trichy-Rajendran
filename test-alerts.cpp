cc64c13b37f21de9aa7a4468f0a2b60b13953a51
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-08 21:11:25 +0530

    added a logic to pass the test

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,6 +6,7 @@
 
 TEST_CASE("detect the ranges and check if the number of readings in each range matches the expected value.") {
   int input_range[2]= {4,5};
-  const char *expected_output = "4-5,2";
-  REQUIRE(strcmp(findReadingsFromRange(input_range), expected_output) == 0);
+  const char *expected_range = "4-5,2";
+  char *actual_range = findReadingsFromRange(input_range);
+  REQUIRE(strcmp(actual_range, expected_output) == 0);
 }
