a4f4db62e05343a68300626ea6ee93e6b5cdf272
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-14 22:51:52 +0530

    new test case with different input and corresponding implementation

--- detect_ranges.c
+++ detect_ranges.c
@@ -1,10 +1,30 @@
 #include "detect_ranges.h"
 #include <stdio.h>
 #include <cstddef>
-char output_string[10] = {0};
+#include <stdlib.h>
+char output_string[20] = {0};
 
-char* findReadingsFromRange (int *input)
+void sortInAscendingOrder (int *sorted_in, int size)
 {
-	sprintf (output_string, "%d-%d,%d", input[0],input[1], 2);
+	int outer_loop, inner_loop, temp;
+	for (outer_loop = 0; outer_loop < size -1; outer_loop++)
+	{
+		for (inner_loop = outer_loop+1; inner_loop < size; inner_loop++)//3,3,4,5
+		{
+			if (sorted_in[outer_loop] > sorted_in[inner_loop])
+			{
+				temp    		 		= sorted_in[outer_loop];
+				sorted_in[outer_loop]   = sorted_in[inner_loop];
+				sorted_in[inner_loop] 	= temp;
+			}
+		}	
+	}
+}
+
+char* findReadingsFromRange (int *in_arr, int size)
+{
+	sortInAscendingOrder(in_arr, size);
+	
+	sprintf (output_string, "%d-%d,%d", in_arr[0],in_arr[size-1], size);
 	return output_string;
 }
--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,9 +4,20 @@
 #include "detect_ranges.h"
 #include <string.h>
 
-TEST_CASE("detect the ranges and check if the number of readings in each range matches the expected value.") {
+TEST_CASE("With provided input range, check if min-max range & number of readings matches the expected value.") {
   int input_range[2]= {4,5};
   const char *expected_range = "4-5,2";
-  char *actual_range = findReadingsFromRange(input_range);
+  int size = sizeof(input_range)/sizeof(input_range[0]);
+  char *actual_range = findReadingsFromRange(input_range, size);
   REQUIRE(strcmp(actual_range, expected_range) == 0);
 }
+
+TEST_CASE("detect the ranges and check if the number of readings matches the expected value.") {
+  int input_range[]= {3, 5, 4, 3};
+  const char *expected_range = "3-5,4";
+  int no_of_elements = sizeof(input_range)/sizeof(input_range[0]);
+  char *actual_range = findReadingsFromRange(input_range, no_of_elements);
+  REQUIRE(strcmp(actual_range, expected_range) == 0);
+}
+
+
