e1e938275759d8cf8df91b83ff1089e4ce618de6
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-21 20:42:09 +0530

    used qsort instead of user-defined sort

--- detect_ranges.c
+++ detect_ranges.c
@@ -5,21 +5,13 @@
 #include <stdlib.h>
 char output_string[20] = {0};
 
-void sortInAscendingOrder (int *sorted_in, int size)
+int ascendingOrder (const void * value_1, const void * value_2) {
+   return ( *(int*)value_1 - *(int*)value_2 );
+}
+
+void sortInOrder (int *sorted_in, int size)
 {
-	int outer_loop, inner_loop, temp;
-	for (outer_loop = 0; outer_loop < size -1; outer_loop++)
-	{
-		for (inner_loop = outer_loop+1; inner_loop < size; inner_loop++)
-		{
-			if (sorted_in[outer_loop] > sorted_in[inner_loop])
-			{
-				temp    		 		= sorted_in[outer_loop];
-				sorted_in[outer_loop]   = sorted_in[inner_loop];
-				sorted_in[inner_loop] 	= temp;
-			}
-		}	
-	}
+	qsort(sorted_in, size, sizeof(int), ascendingOrder);
 }
 
 char* generateReadingsInEachRange (int *in_arr, int size, int first_element, char *result)
@@ -30,7 +22,7 @@ char* generateReadingsInEachRange (int *in_arr, int size, int first_element, cha
 	{
 		if ((in_arr[i+1] - in_arr[i]) > 1)
 		{
-			sprintf (output_string, "%d-%d,%d;", in_arr[first_element],in_arr[i], i+1-first_element);
+			sprintf (output_string, "%d-%d,%d\n", in_arr[first_element],in_arr[i], i+1-first_element);
 			first_element = i+1;
 			strcat(result, output_string);
 			memset(output_string, '\0', sizeof(output_string));
@@ -43,7 +35,7 @@ char* generateReadingsInEachRange (int *in_arr, int size, int first_element, cha
 
 char* getRangeAndReadings (int *in_arr, int size, char *result)
 {
-	sortInAscendingOrder(in_arr, size);
+	sortInOrder(in_arr, size);
 
 	return generateReadingsInEachRange(in_arr, size, FIRST_ELEMENT, result);
 }
