e58e6dc8a175a7ddf201623cf8c421355ec48672
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-28 23:56:02 +0530

    commented duplicate test case

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 
-TEST_CASE("With provided input range, check if min-max range & number of readings matches the expected value.") {
+/* TEST_CASE("With provided input range, check if min-max range & number of readings matches the expected value.") {
   int input_range[2]= {4,5};
   const char *expected_range = "4-5,2";
   char readingRange[OUTPUT_SIZE];
@@ -22,7 +22,7 @@ TEST_CASE("detect the ranges and check if the number of readings matches the exp
   int no_of_elements = sizeof(input_range)/sizeof(input_range[0]);
   char *actual_result = getRangeAndReadings(input_range, no_of_elements, readingRange);
   REQUIRE(strcmp(actual_result, expected_range) == 0);
-}
+} */
 
 TEST_CASE("detect multiple ranges and check if the number of readings of each range matches the expected value.") {
   int input_range[]= {3, 5, 4, 3, 8, 10, 9, 7};
