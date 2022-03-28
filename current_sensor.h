14a7d38960c2189f686d7d8044ff16ffa3bcddaa
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-28 23:05:30 +0530

    compiler error fix

--- current_sensor.h
+++ current_sensor.h
@@ -1,4 +1,4 @@
 #pragma once
 
 enum adc_output {ADC_12BIT=4096, ADC_10bit = 1024};
-int convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputInAmps);
+void convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputInAmps);
