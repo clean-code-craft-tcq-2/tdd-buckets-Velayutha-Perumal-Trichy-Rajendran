a9b12ac0b36a23f1ef6392a86048205bf83a550d
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-28 23:22:46 +0530

    conversion to Amps updated to make test pass

--- current_sensor.c
+++ current_sensor.c
@@ -5,9 +5,11 @@
 void convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputInAmps)
 {
 	int i;
+	float adcValue;
 	for (i = 0; i < no_of_readings; i++)
 	{
-		outputInAmps[i] = (a2d_output[i]/ADC_12BIT) *10;
+		adcValue = a2d_output[i];
+		outputInAmps[i] = (adcValue/ADC_12BIT) *10;
 	}
 	return;
 }
