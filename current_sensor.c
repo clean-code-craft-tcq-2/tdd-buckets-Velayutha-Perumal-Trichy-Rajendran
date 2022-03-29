920eca1527ca9151d7c231c674af61dcd8ad8da0
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-30 00:20:18 +0530

    added -ve readings to return

--- current_sensor.c
+++ current_sensor.c
@@ -7,7 +7,7 @@ void convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputI
 	float adcValue;
 	int max_limit = (1 << adc_bit) - 1;
 	int max_current = max_range - min_range;
-	
+	printf ("max_limit %d\n max_current %d\n",max_limit, max_current);
 	for (reading_index = 0; reading_index < no_of_readings; reading_index++)
 	{
 		if (a2d_output[reading_index] > max_limit)
@@ -16,7 +16,7 @@ void convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputI
 		}
 		else{
 			adcValue = a2d_output[reading_index];
-			outputInAmps[reading_index] = ((adcValue/max_limit) * max_current) + 0.5;
+			outputInAmps[reading_index] = FLOAT_TO_INT(((adcValue/max_limit) * max_current) + min_range);
 		}
 	}
 	return;
