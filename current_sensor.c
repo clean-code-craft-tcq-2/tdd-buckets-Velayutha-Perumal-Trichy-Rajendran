2dbb1c2337fc3ecbfa06e5f9c75fb8b54461d75e
Trichy Rajendran Velayutha Perumal (MS/EAC2-AE) <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-03-29 21:28:39 +0530

    implemented functionality for the same test case

--- current_sensor.c
+++ current_sensor.c
@@ -5,10 +5,18 @@ void convertA2DOutputIntoAmps (int *a2d_output, int no_of_readings, int *outputI
 {
 	int i;
 	float adcValue;
+	int max_range = 1<<12;
+	
 	for (i = 0; i < no_of_readings; i++)
 	{
-		adcValue = a2d_output[i];
-		outputInAmps[i] = ((adcValue/ADC_12BIT) *10) + 0.5;
+		if (a2d_output[i] > max_range)
+		{
+			outputInAmps[i] = -1;
+		}
+		else{
+			adcValue = a2d_output[i];
+			outputInAmps[i] = ((adcValue/ADC_12BIT) *10) + 0.5;
+		}
 	}
 	return;
 }
